565200f4f610174675572d94676ca6d22d4a9b94
Rasha <badranrasha685@gmail.com>
2023-04-07 13:18:16 +0300

    structure

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+require("dotenv").config();
+ const mongoose = require("mongoose");
+const connection = require("./config/db");
+const express = require("express");
+const app = express();
+const cors = require('cors');
 
+
+mongoose.set("strictQuery", true);
+
+connection();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+const port = process.env.PORT || 8080;
+app.listen(port, console.log(`Listening on port ${port}...`));
